48426ec887b6ff4c643f43b73d1fc0a96a02b18e
lakshyajit <lakshyajit165@gmail.com>
2020-02-06 10:54:04 +0530

    initial commit

---
+++ src/main/java/com/lakshyajit/springsqlcrud/SpringsqlcrudApplication.java
@@ -0,0 +1,13 @@
+package com.lakshyajit.springsqlcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsqlcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsqlcrudApplication.class, args);
+	}
+
+}
