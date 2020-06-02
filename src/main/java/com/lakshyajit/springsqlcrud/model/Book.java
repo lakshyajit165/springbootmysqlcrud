cca98a79bc3ac0494b00afdc41b731fb0b400f51
Lakshyajit <lakshyajit165@gmail.com>
2020-06-02 20:33:30 +0530

    docker network host

--- src/main/java/com/lakshyajit/springsqlcrud/model/Book.java
+++ src/main/java/com/lakshyajit/springsqlcrud/model/Book.java
@@ -8,15 +8,20 @@ public class Book {
     @Id
     @GeneratedValue
     private Long id;
+
     @NotBlank
     private String book_name;
+
     @NotBlank
     private String author_name;
+
     @NotBlank
     private String isbn;
+
     public Book(){
         super();
     }
+
     public Book(Long id, String book_name, String author_name, String isbn) {
         super();
         this.id = id;
@@ -24,27 +29,35 @@ public class Book {
         this.author_name = author_name;
         this.isbn=isbn;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getBook_name() {
         return book_name;
     }
+
     public void setBook_name(String book_name) {
         this.book_name = book_name;
     }
+
     public String getAuthor_name() {
         return author_name;
     }
+
     public void setAuthor_name(String author_name) {
         this.author_name = author_name;
     }
+
     public String getIsbn() {
         return isbn;
     }
+    
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
